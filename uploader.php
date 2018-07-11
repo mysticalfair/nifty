83a8fa5dea383a0c73ddc3e47ca6d894a0e74b32
Joanna Li <moochmooche@gmail.com>
2018-07-11 00:33:45 -0400

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -7,7 +7,7 @@ if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
     #echo "apple";
 
     //echo $target_path;
-    $res=shell_exec("python /Users/nus/dataref/datacode.py '".$target_path."' ");
+    $res=shell_exec("python /path/to/file '".$target_path."' ");
     //$output = shell_exec('php download.php');
     #echo $res;
     echo "<a href='" .$res. "'> Click to download </a>";
